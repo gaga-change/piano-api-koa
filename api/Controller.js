4d1091813567d4c4a04233fcb85f6da2de144695
严俊东 <1172482914@qq.com>
2020-04-08 14:08:02 +0800

    登录控制

--- api/Controller.js
+++ api/Controller.js
@@ -1,75 +1,55 @@
+const code = require('./code')
+
 class Controller {
   constructor(model) {
     this.Model = model
   }
 
-  success(ctx, data) {
-    ctx.body = {
-      code: 200,
-      data,
-    }
-  }
-
-  error(ctx, errMsg) {
-    ctx.body = {
-      errMsg
-    }
-  }
-
-
   async create(ctx) {
     let item = ctx.request.body;
 
     item = new this.Model(item);
-    this.success(ctx, await item.save())
+    ctx.body = await item.save()
   }
 
   async destroy(ctx) {
     const { id } = ctx.params;
 
     await this.Model.deleteOne({ _id: id })
-    this.success(ctx)
   }
 
   async update(ctx) {
     const { id } = ctx.params;
     const item = ctx.request.body;
     const model = await this.Model.findById(id)
-    if (model) {
-      await this.Model.update({ _id: id }, item)
-      this.success(ctx)
-    } else {
-      this.error(ctx, "数据已被删除！")
-    }
+    ctx.assert(model, code.BadRequest, "数据已被删除！")
+    await this.Model.update({ _id: id }, item)
   }
 
   async show(ctx) {
     const { id } = ctx.params;
     const model = await this.Model.findById(id)
-    if (model) {
-      this.success(ctx, await this.Model.findById(id))
-    } else {
-      this.error(ctx, "数据已被删除！")
-    }
+    ctx.assert(model, code.BadRequest, "数据已被删除！")
+    ctx.body = await this.Model.findById(id)
   }
 
   async index(ctx) {
     const query = ctx.query;
-    const pageSize = Number(ctx.query.pageSize) || 20;
-    const page = Number(ctx.query.pageNum) || 1;
-    const params = { ...query };
-    delete params.pageSize;
-    delete params.pageNum;
-    const list = await this.Model.find(params)
+    const pageSize = Number(ctx.query.pageSize) || 20
+    const page = Number(ctx.query.pageNum) || 1
+    const params = { ...query }
+    delete params.pageSize
+    delete params.pageNum
+    const res1 = this.Model.find(params)
       .sort({ createdAt: -1 })
       .limit(pageSize)
-      .skip((page - 1) * pageSize);
-    this.success(ctx, {
-      total: await this.Model.countDocuments(params),
-      list,
-    })
+      .skip((page - 1) * pageSize)
+    const res2 = this.Model.countDocuments(params)
+    ctx.body = {
+      total: await res2,
+      list: await res1,
+    }
   }
-
 }
 
 module.exports = Controller
