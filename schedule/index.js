6a7f26a4e8cbe25f38a636e99bf041c6d04ef513
严俊东 <1172482914@qq.com>
2020-05-02 00:17:36 +0800

    空闲时间数据修正

--- schedule/index.js
+++ schedule/index.js
@@ -2,12 +2,20 @@
 
 
 const schedule = require('node-schedule');
+const SpaceArea = require('../api/models/SpaceArea')
 const spaceAreaController = require('../api/spaceArea')
+
+// 数据修正
+const temp = async () => {
+  // 删除老数据
+  await SpaceArea.deleteMany({})
+}
 
 const scheduleCronstyle = () => {
   schedule.scheduleJob('0 0 1 * * *', async () => {
     console.log('定时任务脚本...', new Date().toLocaleString())
     console.log('scheduleCronstyle:' + new Date());
+    await temp()
     const res = await spaceAreaController.autoCreateService()
     console.log(`自动新增空闲时间数量：${res}`)
   });
